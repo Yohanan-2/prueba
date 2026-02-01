bc6268132b50f85d6b2961f82c1ade0d7efa40da
Jesus Yohanan <yohananzempoaltecatllima@gmail.com>
2026-02-01 02:16:51 -0600

    una prueba

---
+++ 3_condicional_if.py
@@ -0,0 +1,12 @@
+#Jesus yohanan Zempoaltecatl Lima 
+#Materia Orgnizacion y estructurado de datos
+numero=int(input("Inserta un numero: "))
+if numero==10:
+    print("X es 10")
+    
+if numero>10:
+    print("X es mayor a 10")
+if numero<10:
+    print("X es menor a 10")
+    
+print("paso por aqui")
